6d491f8b0eb035bb6d9497b36ac3899db83301b3
adrian-lin-1-0-0 <adrian-lin-1-0-0@gmail.com>
2023-09-08 21:01:52 +0800

    feat : mysql repo

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+
 	"github.com/adrian-lin-1-0-0/go-ca/service/delivery/http/v2"
+	"github.com/adrian-lin-1-0-0/go-ca/service/repository/mysql"
 	"github.com/adrian-lin-1-0-0/go-ca/service/usecase"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+func init() {
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("dotenv")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Fatal error config file: %v\n", err)
+	}
+}
 
-	engine := gin.Default()
+func main() {
+	dbHost := viper.GetString("DB_HOST")
+	dbDatabase := viper.GetString("DB_DATABASE")
+	dbUser := viper.GetString("DB_USER")
+	dbPwd := viper.GetString("DB_PASSWORD")
+	dbPort := viper.GetString("DB_PORT")
+
+	db, err := sql.Open(
+		"mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPwd, dbHost, dbPort, dbDatabase),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	petRepo := mysql.NewPetRepository(db)
 
 	petUsecase := usecase.NewPetUsecase(
 		&usecase.PetUsecaseOptions{
-			PetRepo: nil,
+			PetRepo: petRepo,
 		},
 	)
+
+	engine := gin.Default()
 
 	http.RegisterPetHandler(&http.PetHandlerOptions{
 		Engine:  engine,
---
+++ service/repository/sqlite/pet.go
@@ -0,0 +1,55 @@
+package sqlite
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/adrian-lin-1-0-0/go-ca/domain"
+)
+
+type Pet struct {
+	db *sql.DB
+}
+
+func NewPetRepository(db *sql.DB) domain.PetRepository {
+	return &Pet{
+		db: db,
+	}
+}
+
+func (p *Pet) GetPetById(ctx context.Context, id string) (*domain.Pet, error) {
+	row := p.db.QueryRow("SELECT * FROM pet WHERE id = $1", id)
+	var pet domain.Pet
+	err := row.Scan(&pet.Id, &pet.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &pet, nil
+}
+
+func (p *Pet) GetPet(ctx context.Context) ([]*domain.Pet, error) {
+	rows, err := p.db.Query("SELECT * FROM pet")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var pets []*domain.Pet
+	for rows.Next() {
+		var pet domain.Pet
+		err := rows.Scan(&pet.Id, &pet.Name)
+		if err != nil {
+			return nil, err
+		}
+		pets = append(pets, &pet)
+	}
+	return pets, nil
+}
+
+func (p *Pet) AddPet(ctx context.Context, pet *domain.Pet) error {
+	_, err := p.db.Exec("INSERT INTO pet (name) VALUES ($1)", pet.Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
